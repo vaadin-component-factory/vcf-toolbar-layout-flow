01f194a3b83c316bbf674ef4963389630e9e0d0a
Paola De Bartolo <paola.debartolo@gmail.com>
2026-01-19 17:19:41 -0300

    refactor(demo): remove use of flow-component-demo-helpers
    The helper is no compatible with Vaadin 25.

--- vcf-toolbar-layout-flow-demo/src/main/java/org/vaadin/addons/componentfactory/demo/Application.java
+++ vcf-toolbar-layout-flow-demo/src/main/java/org/vaadin/addons/componentfactory/demo/Application.java
@@ -1,10 +1,13 @@
 package org.vaadin.addons.componentfactory.demo;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@StyleSheet(Lumo.STYLESHEET)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
